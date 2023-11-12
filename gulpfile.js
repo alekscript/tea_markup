485da2fa84e6ca502b0c7adae3cfd674a0972a0d
alekscript <piatopal.alex@gmail.com>
2023-11-12 11:44:04 +0100

    start template

---
+++ gulpfile.js
@@ -0,0 +1,113 @@
+const {src, dest, watch, parallel, series} = require('gulp');
+
+const browserSync = require('browser-sync').create();
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const autoprefixer = require('gulp-autoprefixer');
+const clean = require('gulp-clean');
+const avif = require('gulp-avif');
+const webp = require('gulp-webp');
+const imagemin = require('gulp-imagemin');
+const newer = require('gulp-newer');
+const fonter = require('gulp-fonter');
+const ttf2woff2 = require('gulp-ttf2woff2');
+const include = require('gulp-include');
+
+function pages() {
+  return src('app/pages/*.html')
+    .pipe(include({
+      includePaths: 'app/components'
+    }))
+    .pipe(dest('app'))
+    .pipe(browserSync.stream())
+}
+
+function fonts() {
+  return src('app/fonts/src/*.*')
+    .pipe(fonter({
+      formats: ['woff', 'ttf']
+    }))
+    .pipe(src('app/fonts/*.ttf'))
+    .pipe(ttf2woff2())
+    .pipe(dest('app/fonts/'))
+
+}
+
+function images() {
+  return src(['app/images/src/*.*', '!app/images/src/*.svg'])
+    .pipe(newer('app/images/dist'))
+    .pipe(avif({ quality: 50}))
+
+    .pipe(src('app/images/src/*.*'))
+    .pipe(newer('app/images/dist'))
+    .pipe(webp())
+
+    .pipe(src('app/images/src/*.*'))
+    .pipe(newer('app/images'))
+    .pipe(imagemin())
+
+    .pipe(dest('app/images'))
+}
+
+
+function watching() {
+  browserSync.init({
+    server: {
+      baseDir: "app/"
+    }
+  });
+  watch(['app/scss/style.scss'], styles)
+  watch(['app/images/src'], images)
+  watch(['app/js/main.js'], scripts)
+  watch(['app/components/*', 'app/pages/*'], pages)
+  watch(['app/**/*.html']).on('change',browserSync.reload)
+}
+
+function styles() {
+  return src('app/scss/style.scss')
+    .pipe(autoprefixer ({ overrideBrowserslist: ['last 10 version']}))
+    .pipe(concat('style.min.css'))
+    .pipe(scss({outputStyle: 'compressed'}))
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream())
+}
+
+function scripts() {
+  return src([
+    // 'node_modules/swiper/swiper-bundle.js', раскоментировать при надобности
+    'app/js/main.min.js'
+  ])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream())
+}
+
+function buildDist() {
+  return src([
+    'app/css/style.min.css',
+    'app/js/main.min.js',
+    'app/images/*.*',
+    'app/fonts/*.*',
+    'app/**/*.html'], {base: 'app'})
+      .pipe(dest('dist'))
+}
+
+function cleanDist() {
+  return src('dist')
+    .pipe(clean())
+}
+
+exports.watching = watching;
+exports.cleanDist = cleanDist;
+exports.buildDist = buildDist;
+exports.styles = styles;
+exports.images = images;
+exports.fonts = fonts;
+exports.pages = pages;
+exports.scripts = scripts;
+
+
+exports.build = series(cleanDist, images, buildDist);
+exports.default = parallel(styles, scripts, pages, watching);
